63ee46fe15e68d0f67ba77c658c0eb509ab99e16
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2022-12-09 21:38:31 +0000

    Add error context using phantom types

--- native/test/src/lib.rs
+++ native/test/src/lib.rs
@@ -4,47 +4,64 @@ use std::io::{Error as IoError, ErrorKind as IoErrorKind, Write};
 use libftd2xx::{FtStatus, Ftdi, FtdiCommon};
 use rustler::{init, nif, Encoder};
 
-pub struct MyError {
+trait ErrorContext {
+    fn describe<'a>() -> &'a str;
+}
+pub struct MyError<Context> {
     message: String,
+    phantom: std::marker::PhantomData<Context>,
 }
 
-impl From<FtStatus> for MyError {
+impl<C: ErrorContext> From<FtStatus> for MyError<C> {
     fn from(error: FtStatus) -> Self {
         Self {
             message: match error {
-                FtStatus::DEVICE_NOT_FOUND => "FTDI device not found".into(),
+                FtStatus::DEVICE_NOT_FOUND => {
+                    format!("Error {}: FTDI device not found", C::describe())
+                }
                 _ => format!("Error: {}", error),
             },
+            phantom: std::marker::PhantomData::<C>,
         }
     }
 }
 
 // If specialisation (RFC 1210) is implemented we can do a generic
 // implementation for any error
-impl From<IoError> for MyError {
+impl<C: ErrorContext> From<IoError> for MyError<C> {
     fn from(error: IoError) -> Self {
         Self {
             message: match error.kind() {
-                IoErrorKind::PermissionDenied => format!("Permission denied: {}", error),
+                IoErrorKind::PermissionDenied => {
+                    format!("Error in {}: permission denied: {}", C::describe(), error)
+                }
                 _ => format!("Error: {}", error),
             },
+            phantom: std::marker::PhantomData::<C>,
         }
     }
 }
 
-impl Encoder for MyError {
+impl<T> Encoder for MyError<T> {
     fn encode<'a>(&self, env: rustler::Env<'a>) -> rustler::Term<'a> {
         self.message.encode(env)
     }
 }
 
-impl From<MyError> for rustler::Error {
-    fn from(my_error: MyError) -> Self {
+impl<T> From<MyError<T>> for rustler::Error {
+    fn from(my_error: MyError<T>) -> Self {
         rustler::Error::RaiseTerm(Box::new(my_error.message))
     }
 }
 
-fn _open() -> Result<(), MyError> {
+pub struct OpenContext;
+impl ErrorContext for OpenContext {
+    fn describe<'a>() -> &'a str {
+        "opening the FTDI device"
+    }
+}
+
+fn _open() -> Result<(), MyError<OpenContext>> {
     let mut device = Ftdi::new()?;
     let info = device.device_info()?;
     let mut info_file = File::create("device_info.txt")?;
@@ -54,7 +71,7 @@ fn _open() -> Result<(), MyError> {
 }
 
 #[nif]
-pub fn open(_args: rustler::Term) -> Result<(), MyError> {
+pub fn open(_args: rustler::Term) -> Result<(), MyError<OpenContext>> {
     _open()
 }
 
