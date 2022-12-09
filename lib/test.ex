c6d7ae2ddccdf3cec766ad57a192f1bdb1465598
Robert Kovacsics <robert.kovacsics@cambridgeconsultants.com>
2022-12-09 22:15:12 +0000

    Use `getenv` instead of `libftd2xx`

--- lib/test.ex
+++ lib/test.ex
@@ -6,7 +6,7 @@ defmodule Test do
   # When loading a NIF module, dummy clauses for all NIF function are required.
   # NIF dummies usually just error out when called when the NIF is not loaded,
   # as that should never normally happen.
-  def open(_args), do: :erlang.nif_error(:nif_not_loaded)
+  def getenv(_args), do: :erlang.nif_error(:nif_not_loaded)
 
-  def open_bang(_args), do: :erlang.nif_error(:nif_not_loaded)
+  def getenv_bang(_args), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/test/src/lib.rs
+++ native/test/src/lib.rs
@@ -1,18 +1,18 @@
+use std::env::{var, VarError};
 use std::fs::File;
 use std::io::{Error as IoError, ErrorKind as IoErrorKind, Write};
 
-use libftd2xx::{FtStatus, Ftdi, FtdiCommon};
 use rustler::{init, nif, Encoder};
 
 pub struct MyError {
     message: String,
 }
 
-impl From<FtStatus> for MyError {
-    fn from(error: FtStatus) -> Self {
+impl From<VarError> for MyError {
+    fn from(error: VarError) -> Self {
         Self {
             message: match error {
-                FtStatus::DEVICE_NOT_FOUND => "FTDI device not found".into(),
+                VarError::NotPresent => format!("Variable not set: {}", error),
                 _ => format!("Error: {}", error),
             },
         }
@@ -44,23 +44,26 @@ impl From<MyError> for rustler::Error {
     }
 }
 
-fn _open() -> Result<(), MyError> {
-    let mut device = Ftdi::new()?;
-    let info = device.device_info()?;
-    let mut info_file = File::create("device_info.txt")?;
-    write!(info_file, "Device information: {:?}", info)?;
-    println!("Device information: {:?}", info);
-    Ok(())
+fn _getenv(key: String) -> Result<String, MyError> {
+    let val = var(&key)?;
+    let mut info_file = File::create("var_dump.txt")?;
+    write!(
+        info_file,
+        "Environment variable {:?} has value {:?}",
+        key, val
+    )?;
+    println!("Environment variable {:?} has value {:?}", key, val);
+    Ok(val)
 }
 
 #[nif]
-pub fn open(_args: rustler::Term) -> Result<(), MyError> {
-    _open()
+pub fn getenv(key: String) -> Result<String, MyError> {
+    _getenv(key)
 }
 
 #[nif]
-pub fn open_bang(_args: rustler::Term) -> rustler::NifResult<()> {
-    Ok(_open()?)
+pub fn getenv_bang(key: String) -> rustler::NifResult<String> {
+    Ok(_getenv(key)?)
 }
 
-init!("Elixir.Test", [open, open_bang]);
+init!("Elixir.Test", [getenv, getenv_bang]);
